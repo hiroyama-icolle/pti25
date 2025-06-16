1f6d349e6e095c61dc9174f624683ad5f6feb86b
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2025-06-16 08:01:42 +0000

    250616

---
+++ def2.py
@@ -0,0 +1,7 @@
+def postTaxPrice(price):
+    ans = price * 1.1
+    return ans
+
+print(postTaxPrice(120),"円")
+print(128)#←続きを作ってください
+print(980)#←続きを作ってください
---
+++ for1.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    print(5, "x",i,"=",5*i)
+
+
